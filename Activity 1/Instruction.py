e541d4f745129ae684b36837c265828d11a5c931
Hunya-syete <tinangmarcillones@gmail.com>
2024-02-01 09:57:51 +0800

    Please pass

---
+++ Activity 1/Instruction.py
@@ -0,0 +1,23 @@
+# Activity 1 (Assignment)
+
+# Write a program that will compute for the students average
+# The use will input the following
+# Name
+# Math
+# Science
+# English
+# If the average is equal and above 75 	status is Passed else You Failed the 	semester
+
+
+# Name: Bien Math: 99
+# Science: 92
+# English: 74
+# Average: 88.0
+
+# Congratulations! You passed the semester. But you need to re-enroll English subject
+
+# Sorry, You Failed the semester
+
+
+# DEADLINE 9:30AM 2/1/2024
+# lastName.py
---
+++ Activity 2/Instruction.py
@@ -0,0 +1,9 @@
+# Activity 2
+
+# Write a program that will check the employees years in service and office.
+# The user will input number for years and in service and the following offices (it, acct, hr)
+# Check the following conditions
+
+
+# DEADLINE: 11:30AM 2/1/2024
+# lastName.py
