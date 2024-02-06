fc3b13b4d11edb90492698bce18ac50b6da5dbf1
Hunya-syete <tinangmarcillones@gmail.com>
2024-02-06 11:04:05 +0800

    Please pass today

--- Activity 4/instructions.py
+++ Activity 4/instructions.py
@@ -1,10 +1,7 @@
-# Create a simple "Guess the Number" game using a loop. The program generates a random number, and the user has to guess it within a limited number of attempts.
+# Create a class called Car. The Car class has the following fields:
+# color
+# model
+# Manufacturer
 
-# Requirements:
-# Generate a random number between 1 and 20.
-# Allow the user to guess the number.
-# Provide feedback on whether the guess is correct, too high, or too low.
-# Allow the user to keep guessing until they reach a maximum of 3 attempts.
-# Use a loop for the guessing process.
 
-# DEADLINE 10:10AM.
+# Instantiate the Car class 3 times and display all properties Modify the properties
--- Activity 5/instructions.py
+++
@@ -1,7 +0,0 @@
-# Create a class called Car. The Car class has the following fields:
-# color
-# model
-# Manufacturer
-
-
-# Instantiate the Car class 3 times and display all properties Modify the properties
