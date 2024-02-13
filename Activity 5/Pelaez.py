9fdd01380e8a0cfe034995f3565a93b25c8a5c1d
ohquinnarzel <158390188+ohquinnarzel@users.noreply.github.com>
2024-02-13 19:27:03 +0800

    Create Pelaez.py

---
+++ Activity 5/Pelaez.py
@@ -0,0 +1,9 @@
+import numpy as np
+from IPython.display import Audio, display
+
+duration, sampling_freq, frequency = 5.0, 44100, 440.0
+
+t = np.arange(0, duration, 3/sampling_freq)
+sound_wave = 0.5 * np.sign(np.sin(2 * np.pi * frequency * t))  # Square wave
+
+display(Audio(data=sound_wave, rate=sampling_freq))
