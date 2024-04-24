975fe67442f29cbe166027c260e2e2c44abf90ed
kitzammad <158400113+kitzammad@users.noreply.github.com>
2024-04-24 16:48:26 +0800

    Create KitzActivity8.py

---
+++ Activity 8/KitzActivity8.py
@@ -0,0 +1,12 @@
+from thinkdsp import SquareSignal
+from thinkdsp import decorate
+
+signal = SquareSignal(1100)
+duration = signal.period*3
+segment = signal.make_wave(duration, framerate=40000)
+segment.plot()
+decorate(xlabel='Time (s)')
+
+wave = signal.make_wave(duration=20, framerate=99000)
+wave.apodize()
+wave.make_audio()
